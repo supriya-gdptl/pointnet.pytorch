0d45b36072a4c20f37e60ae0522f5aedd89dbcbd
supriya-pandhre <supriya.pandhre9@gmail.com>
2021-01-26 14:50:49 -0800

    write logfile and store loss, acc in file

--- misc/common_functions.py
+++ misc/common_functions.py
@@ -20,8 +20,19 @@ def replace_file_extension(input_file):
         print(f"Extension .ply replaced with .off in file {input_file}")
 
 
-if __name__ == '__main__':
-    dataset_folder = "../../../../modelnet40/ModelNet40"#sys.argv[1]
+def write_log(logfile, msg):
+    logfile.write(f'{msg}\n')
+    print(msg)
+    logfile.flush()
+
+
+def write_loss_acc(loss_acc_file, mode, epoch, loss, acc):
+    loss_acc_file.write(f'{mode},{epoch},{loss:.6f},{acc:.6f}\n')
+    loss_acc_file.flush()
+
+def main():
+    # if __name__ == '__main__':
+    dataset_folder = sys.argv[1]
     replace_file_extension(os.path.join(dataset_folder, 'train.txt'))
     replace_file_extension(os.path.join(dataset_folder, 'val.txt'))
     replace_file_extension(os.path.join(dataset_folder, 'test.txt'))
--- utils/train_classification.py
+++ utils/train_classification.py
@@ -2,125 +2,152 @@ from __future__ import print_function
 import argparse
 import os
 import random
-import torch
-import torch.nn.parallel
+import time
 import torch.optim as optim
 import torch.utils.data
 from pointnet.dataset import ShapeNetDataset, ModelNetDataset
 from pointnet.model import PointNetCls, feature_transform_regularizer
 import torch.nn.functional as F
 from tqdm import tqdm
+import numpy as np
+
+from misc.common_functions import write_loss_acc, write_log
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    '--batchSize', type=int, default=32, help='input batch size')
-parser.add_argument(
-    '--num_points', type=int, default=2500, help='input batch size')
-parser.add_argument(
-    '--workers', type=int, help='number of data loading workers', default=4)
-parser.add_argument(
-    '--nepoch', type=int, default=250, help='number of epochs to train for')
-parser.add_argument('--outf', type=str, default='cls', help='output folder')
-parser.add_argument('--model', type=str, default='', help='model path')
-parser.add_argument('--dataset', type=str, required=True, help="dataset path")
-parser.add_argument('--dataset_type', type=str, default='shapenet', help="dataset type shapenet|modelnet40")
-parser.add_argument('--feature_transform', action='store_true', help="use feature transform")
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--batchSize', type=int, default=32, help='input batch size')
+    parser.add_argument(
+        '--num_points', type=int, default=2500, help='input batch size')
+    parser.add_argument(
+        '--workers', type=int, help='number of data loading workers', default=4)
+    parser.add_argument(
+        '--nepoch', type=int, default=250, help='number of epochs to train for')
+    parser.add_argument('--outf', type=str, default='cls', help='output folder')
+    parser.add_argument('--model', type=str, default='', help='model path')
+    parser.add_argument('--dataset', type=str, required=True, help="dataset path")
+    parser.add_argument('--dataset_type', type=str, default='shapenet', help="dataset type shapenet|modelnet40")
+    parser.add_argument('--feature_transform', action='store_true', help="use feature transform")
 
-opt = parser.parse_args()
-print(opt)
+    opt = parser.parse_args()
 
-# run misc/common_functions.py to replace model file extensions from .ply to .off in {train/val/test/trainval}.txt
+    # log
+    logtime = time.strftime("%d_%m_%Y_%H_%M_%S")
+    logfile = open(os.path.join(opt.outf, f'log_{logtime}.txt'), 'w')
+    loss_acc_file = open(os.path.join(opt.outf, f'loss_acc_{logtime}.txt'), 'w')
+    loss_acc_file.write("mode,epoch,loss,acc\n")
+    print(f"Saving log file to: {os.path.abspath(logfile.name)}")
 
-blue = lambda x: '\033[94m' + x + '\033[0m'
+    write_log(logfile=logfile,msg=opt)
 
-opt.manualSeed = random.randint(1, 10000)  # fix seed
-print("Random Seed: ", opt.manualSeed)
-random.seed(opt.manualSeed)
-torch.manual_seed(opt.manualSeed)
+    # run misc/common_functions.py to replace model file extensions from .ply to .off in {train/val/test/trainval}.txt
 
-if opt.dataset_type == 'shapenet':
-    dataset = ShapeNetDataset(
-        root=opt.dataset,
-        classification=True,
-        npoints=opt.num_points)
+    opt.manualSeed = random.randint(1, 10000)  # fix seed
+    write_log(logfile=logfile,msg=f"Random Seed: {opt.manualSeed}")
+    random.seed(opt.manualSeed)
+    torch.manual_seed(opt.manualSeed)
 
-    test_dataset = ShapeNetDataset(
-        root=opt.dataset,
-        classification=True,
-        split='test',
-        npoints=opt.num_points,
-        data_augmentation=False)
-elif opt.dataset_type == 'modelnet40':
-    dataset = ModelNetDataset(
-        root=opt.dataset,
-        npoints=opt.num_points,
-        split='trainval')
+    if opt.dataset_type == 'shapenet':
+        dataset = ShapeNetDataset(
+            root=opt.dataset,
+            classification=True,
+            npoints=opt.num_points)
 
-    test_dataset = ModelNetDataset(
-        root=opt.dataset,
-        split='test',
-        npoints=opt.num_points,
-        data_augmentation=False)
-else:
-    exit('wrong dataset type')
+        test_dataset = ShapeNetDataset(
+            root=opt.dataset,
+            classification=True,
+            split='test',
+            npoints=opt.num_points,
+            data_augmentation=False)
+    elif opt.dataset_type == 'modelnet40':
+        dataset = ModelNetDataset(
+            root=opt.dataset,
+            npoints=opt.num_points,
+            split='trainval')
 
+        test_dataset = ModelNetDataset(
+            root=opt.dataset,
+            split='test',
+            npoints=opt.num_points,
+            data_augmentation=False)
+    else:
+        exit('wrong dataset type')
 
-dataloader = torch.utils.data.DataLoader(
-    dataset,
-    batch_size=opt.batchSize,
-    shuffle=True,
-    num_workers=int(opt.workers))
-
-testdataloader = torch.utils.data.DataLoader(
-        test_dataset,
+    dataloader = torch.utils.data.DataLoader(
+        dataset,
         batch_size=opt.batchSize,
         shuffle=True,
         num_workers=int(opt.workers))
 
-print(len(dataset), len(test_dataset))
-num_classes = len(dataset.classes)
-print('classes', num_classes)
+    testdataloader = torch.utils.data.DataLoader(
+            test_dataset,
+            batch_size=opt.batchSize,
+            shuffle=True,
+            num_workers=int(opt.workers))
 
-try:
-    os.makedirs(opt.outf)
-except OSError:
-    pass
+    write_log(logfile=logfile,
+              msg=f"number of training examples: {len(dataset)}\nnumber of test example: {len(test_dataset)}")
+    num_classes = len(dataset.classes)
+    write_log(logfile=logfile,msg=f'number of classes {num_classes}')
 
-classifier = PointNetCls(k=num_classes, feature_transform=opt.feature_transform)
+    try:
+        os.makedirs(opt.outf)
+    except OSError:
+        pass
 
-if opt.model != '':
-    classifier.load_state_dict(torch.load(opt.model))
+    classifier = PointNetCls(k=num_classes, feature_transform=opt.feature_transform)
 
+    if opt.model != '':
+        classifier.load_state_dict(torch.load(opt.model))
 
-optimizer = optim.Adam(classifier.parameters(), lr=0.001, betas=(0.9, 0.999))
-scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.5)
-classifier.cuda()
+    optimizer = optim.Adam(classifier.parameters(), lr=0.001, betas=(0.9, 0.999))
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.5)
+    classifier.cuda()
 
-num_batch = len(dataset) / opt.batchSize
+    num_batch = len(dataset) / opt.batchSize
 
-for epoch in range(opt.nepoch):
-    scheduler.step()
-    for i, data in enumerate(dataloader, 0):
-        points, target = data
-        target = target[:, 0]
-        points = points.transpose(2, 1)
-        points, target = points.cuda(), target.cuda()
-        optimizer.zero_grad()
-        classifier = classifier.train()
-        pred, trans, trans_feat = classifier(points)
-        loss = F.nll_loss(pred, target)
-        if opt.feature_transform:
-            loss += feature_transform_regularizer(trans_feat) * 0.001
-        loss.backward()
-        optimizer.step()
-        pred_choice = pred.data.max(1)[1]
-        correct = pred_choice.eq(target.data).cpu().sum()
-        print('[%d: %d/%d] train loss: %f accuracy: %f' % (epoch, i, num_batch, loss.item(), correct.item() / float(opt.batchSize)))
+    for epoch in range(opt.nepoch):
 
-        if i % 10 == 0:
-            j, data = next(enumerate(testdataloader, 0))
+        scheduler.step()
+        train_loss = []
+        train_acc = []
+
+        for i, data in enumerate(dataloader, 0):
             points, target = data
+            target = target[:, 0]
+            points = points.transpose(2, 1)
+            points, target = points.cuda(), target.cuda()
+            optimizer.zero_grad()
+            classifier = classifier.train()
+            pred, trans, trans_feat = classifier(points)
+            loss = F.nll_loss(pred, target)
+            if opt.feature_transform:
+                loss += feature_transform_regularizer(trans_feat) * 0.001
+            loss.backward()
+            optimizer.step()
+            pred_choice = pred.data.max(1)[1]
+            correct = pred_choice.eq(target.data).cpu().sum()
+
+            train_loss.append(loss.item())
+            train_acc.append(correct.item()/float(opt.batchSize))
+
+            write_log(logfile=logfile,
+                      msg=f'[{epoch}: {i}/{num_batch}] train loss: {loss.item()}, '
+                          f'accuracy: {correct.item()/float(opt.batchSize)}')
+
+        write_log(logfile=logfile,
+                  msg=f'\nTRAIN [{epoch}/{opt.nepoch}] train loss: {np.array(train_loss).mean()}, '
+                      f'accuracy: {np.array(train_acc).mean()}\n')
+
+        write_loss_acc(loss_acc_file=loss_acc_file, mode='train',
+                       loss=np.array(train_loss).mean(), acc=np.array(train_acc).mean(), epoch=epoch)
+
+        # test on test data after one epoch
+        test_loss = []
+        test_acc = []
+        for j, testdata in enumerate(testdataloader, 0):
+            points, target = testdata
             target = target[:, 0]
             points = points.transpose(2, 1)
             points, target = points.cuda(), target.cuda()
@@ -129,22 +156,31 @@ for epoch in range(opt.nepoch):
             loss = F.nll_loss(pred, target)
             pred_choice = pred.data.max(1)[1]
             correct = pred_choice.eq(target.data).cpu().sum()
-            print('[%d: %d/%d] %s loss: %f accuracy: %f' % (epoch, i, num_batch, blue('test'), loss.item(), correct.item()/float(opt.batchSize)))
 
-    torch.save(classifier.state_dict(), '%s/cls_model_%d.pth' % (opt.outf, epoch))
+            test_loss.append(loss.item())
+            test_acc.append(correct.item()/float(opt.batchSize))
 
-total_correct = 0
-total_testset = 0
-for i,data in tqdm(enumerate(testdataloader, 0)):
-    points, target = data
-    target = target[:, 0]
-    points = points.transpose(2, 1)
-    points, target = points.cuda(), target.cuda()
-    classifier = classifier.eval()
-    pred, _, _ = classifier(points)
-    pred_choice = pred.data.max(1)[1]
-    correct = pred_choice.eq(target.data).cpu().sum()
-    total_correct += correct.item()
-    total_testset += points.size()[0]
+        write_log(logfile=logfile,
+                  msg=f'\nTEST [{epoch}/{opt.nepoch}] loss: {np.array(test_loss).mean()}, '
+                      f'accuracy: {np.array(test_acc).mean()}\n')
+        write_loss_acc(loss_acc_file=loss_acc_file, mode='test',
+                       loss=np.array(test_loss).mean(), acc=np.array(test_acc).mean(), epoch=epoch)
 
-print("final accuracy {}".format(total_correct / float(total_testset)))
+        torch.save(classifier.state_dict(), '%s/cls_model_%d.pth' % (opt.outf, epoch))
+
+    total_correct = 0
+    total_testset = 0
+    for i,data in tqdm(enumerate(testdataloader, 0)):
+        points, target = data
+        target = target[:, 0]
+        points = points.transpose(2, 1)
+        points, target = points.cuda(), target.cuda()
+        classifier = classifier.eval()
+        pred, _, _ = classifier(points)
+        pred_choice = pred.data.max(1)[1]
+        correct = pred_choice.eq(target.data).cpu().sum()
+        total_correct += correct.item()
+        total_testset += points.size()[0]
+
+    write_log(logfile=logfile,msg=f"final accuracy {total_correct / float(total_testset)}")
+
