f2b7a6e569fe908be1bd50912e611fa097023b8f
supriya-gdptl <supriya.pandhre9@gmail.com>
2021-01-28 18:45:38 -0800

    test on OFF modelnet40 test set

--- utils/show_cls.py
+++ utils/show_cls.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn.parallel
 import torch.utils.data
 from torch.autograd import Variable
-from pointnet.dataset import HDF5_ModelNetDataset
+from pointnet.dataset import HDF5_ModelNetDataset, ModelNetDataset
 from pointnet.model import PointNetCls
 import torch.nn.functional as F
 
@@ -15,17 +15,28 @@ parser.add_argument('--model', type=str, default = '',  help='model path')
 parser.add_argument('--feature_transform', action='store_true', help='feature transform')
 parser.add_argument('--num_points', type=int, default=1024, help='input batch size')
 parser.add_argument('--dataset', type=str, default='', help='dataset path')
+parser.add_argument('--dataset_type', type=str, default='modelnet40', help='choice=[modelnet40, hdf5_modelnet40]')
 
 
 opt = parser.parse_args()
 print(opt)
 
-test_dataset = HDF5_ModelNetDataset(
+if opt.dataset_type == "modelnet40":
+    test_dataset = ModelNetDataset(
             root=opt.dataset,
             split='test',
             npoints=opt.num_points,
             data_augmentation=False)
-print('number of test examples:',len(test_dataset))
+elif opt.dataset_type == "hdf5_modelnet40":
+    test_dataset = HDF5_ModelNetDataset(
+                root=opt.dataset,
+                split='test',
+                npoints=opt.num_points,
+                data_augmentation=False)
+else:
+    print('wrong dataset_type')
+
+print('{}: number of test examples:{}'.format(opt.dataset_type, len(test_dataset)))
 
 testdataloader = torch.utils.data.DataLoader(
     test_dataset, batch_size=1, shuffle=True)
